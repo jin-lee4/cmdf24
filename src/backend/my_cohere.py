f4e2086e0012de699afeadaf2af442236f35ca55
noodnoof <nadianafeesa@gmail.com>
2024-03-09 19:02:55 -0800

    implemented db functions together with cohere api functions

--- src/backend/my_cohere.py
+++ src/backend/my_cohere.py
@@ -1,6 +1,9 @@
+from bson import ObjectId
 import cohere
 import os
 from dotenv import load_dotenv
+from user_db_functions import *
+from chat_db_functions import *
 
 load_dotenv(".env")
 
@@ -10,8 +13,10 @@ class MyCohere:
   """
   def __init__(self):
     self.co = cohere.Client(os.environ.get('COHERE_KEY'))
+    self.chatDB = ChatDb()
+    self.userDB = UserDB()
 
-  def summarize_chat(self, chat_text: str, date_range) -> str:
+  def summarize_chat(self, sender_id: ObjectId, recipient_id: ObjectId, date_from: datetime=None, date_to: datetime=None) -> str:
     """
     Summarize chat messages
     Args:
@@ -20,29 +25,33 @@ class MyCohere:
     Returns:
     - summary_text (str): summarized text in bullet point form
     """
-    # TODO: Extract text from database based on specified date range
-    ## CODE HERE ##
-
+    # Extract text from database based on specified date range
+    messages_list = self.chatDB.get_messages(sender_id, recipient_id, from_date=date_from, to_date=date_to)
+    # Concatenate all messages into a single string
+    chat_text = " ".join([message["message"] for message in messages_list])
     # Call the summarize method from the cohere client
     response = self.co.summarize(text=chat_text, length="medium", format="bullets", extractiveness="auto" )
     # Extract the summarized text from the response tuple (id, summarised text, meta)
     summary_text = response[1]
     return summary_text
 
-  def create_suggested_text(self, last_message: str, sender: str, recipient: str, sender_name: str, recipient_name: str) -> str:
+  def create_suggested_text(self, sender_id: ObjectId, recipient_id: ObjectId) -> str:
     """
     Create a suggested response based on the last message
 
     Args:
-    - last_message (str): the last message in the chat
-    - sender (str): the potential sender of the suggested text (either 'mentor' or 'mentee')
-    - recipient (str): the potential sender of the suggested text (either 'mentor' or 'mentee')
+    - sender_id (str): the id of the potential sender of the suggested text
+    - recipient_id (str): the id of the potential recipient of the suggested text
     
     Returns:
     - suggested_response (str): the suggested response based on the last message
     """
+    # get name of sender and recipient
+    sender_name = self.userDB.get_name(sender_id)
+    recipient_name = self.userDB.get_name(recipient_id)
+
     # Construct the prompt using the last message, sender, and recipient information
-    my_prompt = f"Based on the last message from a {recipient} named {recipient_name}, please suggest a response: {last_message} in the perspective of a {sender} called {sender_name}."
+    my_prompt = f"Based on the last message (sentence) from {recipient_name}, please suggest a response in the perspective of {sender_name}. Only respond with the next sentence, nothing else."
     
     # Generate a response based on the prompt
     generate_output = self.co.generate(prompt=my_prompt)
@@ -85,9 +94,9 @@ if __name__ == "__main__":
   # summary_text = my_cohere.summarize_chat(sample_text, date_range="last 7 days")
   # print(summary_text)
 
-  # test create suggested text function
-  last_message = "I am Nadia, a final year computer science student and I am interested in AI. I would love to chat with you as a mentee."
-  suggested_response = my_cohere.create_suggested_text(last_message, sender="mentor", recipient="mentee", sender_name="John", recipient_name="Nadia")
-  print(suggested_response)
+  # # test create suggested text function
+  # last_message = "I am Nadia, a final year computer science student and I am interested in AI. I would love to chat with you as a mentee."
+  # suggested_response = my_cohere.create_suggested_text(last_message, sender="mentor", recipient="mentee", sender_name="John", recipient_name="Nadia")
+  # print(suggested_response)
 
 
