cdc4d07bb5c19785035918be344c6d5df61a3863
marianeolivan <101530392+marianeolivan@users.noreply.github.com>
2024-03-10 09:34:34 -0700

    landing page bg

--- src/app/page.js
+++ src/app/page.js
@@ -5,23 +5,34 @@ import Logo from '../components/Logo';
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
-      <div className="header min-w-screen flex justify-between my-5 mx-5">
-        <Logo />
-        <span className="">
-          <button>Log In</button>
-        </span>
-      </div>
-      <div className="landing-content">
-        <h1 className="landing-title">Welcome to Orbit</h1>
-        <p className="landing-description py-5">your community for growth</p>
-        <button>Join Now</button>
-        {/* <div className="landing-images">
-          <Image src="C:\Users\jjin_\github\cmdf24\public\images\landing-1.png"
-          width={484.167}
-          height={463.836}/>
-        </div> */}
-      </div>
-    </main>
+
+    <div style={{ 
+      backgroundImage: `url('/images/landing-bg.jpg')`,
+      height: '100vh', // Adjust the height as needed
+      backgroundSize: 'cover', // Cover the entire page
+      backgroundPosition: 'center' // Center the background image
+    }}>
+      <main className="min-h-screen">
+
+<div className="header min-w-screen flex justify-between my-5 mx-5">
+  <Logo />
+  <span className="">
+    <button>Log In</button>
+  </span>
+</div>
+<div className="landing-content">
+  <h1 className="landing-title">Welcome to Orbit</h1>
+  <p className="landing-description py-5">your community for growth</p>
+  <button>Join Now</button>
+  {/* <div className="landing-images">
+    <Image src="C:\Users\jjin_\github\cmdf24\public\images\landing-1.png"
+    width={484.167}
+    height={463.836}/>
+  </div> */}
+</div>
+</main>
+    </div>
+    
+    
   );
 }
---
+++ src/components/ProfilePreview.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "@/app/styles/profilepreview.module.css";
+
+function MessagePreview() {
+  return (
+    <section>
+
+    </section>
+  );
+}
+
+export default ProfilePreview;
